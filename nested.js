a9e69bdbabff00fa148bb94dde5488770cd2ff18
asniyemaryam <61863847+asniyemaryam@users.noreply.github.com>
2020-03-19 13:53:33 -0700

    Add files via upload

---
+++ nested.js
@@ -0,0 +1,14 @@
+for (i = 1; i <= 10; i++) {
+    for (j = 1; j <= i; j++) {
+        document.write(j + " ")
+    }
+    document.write(' <br>')
+}
+document.write(' <br> ')
+
+for (i = 0; i < 11; i++) {
+    for (j = 10; j > i; j--) {
+        document.write(j + ' ')
+    }
+    document.write(' <br>')
+}
